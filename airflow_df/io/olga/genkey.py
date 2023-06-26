f173b87f0481fec8adc48d806c9d6e4cb16707fb
JesusDBS <jesusbingucv@gmail.com>
2023-06-25 21:33:21 -0400

    [REFACTOR] __split_line_elements.

--- airflow_df/io/olga/genkey.py
+++ airflow_df/io/olga/genkey.py
@@ -141,6 +141,10 @@ class Genkey(dict):
 
         return fixed_lines
 
+    @staticmethod
+    def check_if_last_element(element_position: int, element_list: list) -> bool:
+        return element_position + 1 == len(element_list)
+
     def __split_line_elements(self, line: str) -> list:
         """This method split a genkey file line into its different elements.
         Returns a list containing a second-level key and third-level keys and values elements.
@@ -149,59 +153,73 @@ class Genkey(dict):
 
         **line:** (str) Line belonging to a principal genkey-text block. 
         """
+        def separate_second_level_key(line_element: str, split_by=' ', join_by=' ') -> tuple:
+
+            splitted_line = line_element.split(split_by)
+            second_level_key = splitted_line[0]
+            third_level_key_val = join_by.join(list(splitted_line[1:]))
+
+            return second_level_key, third_level_key_val
+
         _info = ''
-        _el = ''
-        clean_line = []
+        previous_element = ''
+        splitted_line = []
         flag = False
 
-        for n, el in enumerate(line.split(', ')):
+        line = line.split(', ')
 
-            if self.regex.GENKEY_SECOND_LEVEL_KEY_PATTERN.search(el):
-                splited_line = el.split(' ')
-                clean_line.append(splited_line[0])
-                second_key = ' '.join([e for e in splited_line[1:]])
+        for element_position, line_element in enumerate(line):
+            if self.regex.GENKEY_SECOND_LEVEL_KEY_PATTERN.search(line_element):
+                second_level_key, third_level_key_val = separate_second_level_key(
+                    line_element=line_element)
+                splitted_line.append(second_level_key)
 
-                if self.regex.THIRD_LEVEL_PARENTHESES_OPENING_VALUE_PATTERN.search(second_key):
-                    _el = second_key
+                if self.regex.THIRD_LEVEL_PARENTHESES_OPENING_VALUE_PATTERN.search(third_level_key_val):
+                    previous_element = third_level_key_val
                     flag = True
                     continue
-                el = second_key
-                clean_line.append(el)
+
+                line_element = third_level_key_val
+                splitted_line.append(line_element)
                 continue
 
-            if self.regex.THIRD_LEVEL_PARENTHESES_OPENING_VALUE_PATTERN.search(el):
-                _el = el
+            if self.regex.THIRD_LEVEL_PARENTHESES_OPENING_VALUE_PATTERN.search(line_element):
+                previous_element = line_element
                 flag = True
                 continue
 
-            if self.regex.GENKEY_THIRD_LEVEL_KEY_PATTERN.search(el):
-                if n + 1 == len(line.split(', ')):
-                    clean_line.append(el)
+            if self.regex.GENKEY_THIRD_LEVEL_KEY_PATTERN.search(line_element):
+                if self.check_if_last_element(element_position, line):
+                    splitted_line.append(line_element)
                     continue
 
-                if self.regex.THIRD_LEVEL_KEY_INFO_PATTERN.search(el):
-                    _info = el
+                if self.regex.THIRD_LEVEL_KEY_INFO_PATTERN.search(line_element):
+                    _info = line_element
                     continue
 
-                clean_line.append(el)
+                splitted_line.append(line_element)
                 continue
 
+            # TODO: Handle when key INFO has a string with ',' for example:
+            # INFO="Modelo de parada, a partir del minuto 9 el sistema queda estable"
+            # This block of code is currently not working.
+            # Fix it or ask to not write the value of INFO in that fashion.
             if self.regex.THIRD_LEVEL_KEY_INFO_PATTERN.search(_info):
-                _info = _info + ', ' + el
-                clean_line.append(_info)
+                _info = _info + ', ' + line_element
+                splitted_line.append(_info)
                 _info = ''
                 continue
 
             if flag:
-                el = ', ' + el
-                _el += el
+                line_element = ', ' + line_element
+                previous_element += line_element
 
-                if self.regex.THIRD_LEVEL_PARENTHESES_CLOSING_VALUE_PATTERN.search(el):
-                    clean_line.append(_el)
-                    _el = ''
+                if self.regex.THIRD_LEVEL_PARENTHESES_CLOSING_VALUE_PATTERN.search(line_element):
+                    splitted_line.append(previous_element)
+                    previous_element = ''
                     flag = False
 
-        return clean_line
+        return splitted_line
 
     @staticmethod
     def remove_string_quotes(string: str) -> str:
